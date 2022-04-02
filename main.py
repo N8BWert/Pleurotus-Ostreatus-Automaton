f29851678deca91e0df6460adf398ac1d0e52f44
Nathaniel Wert <n8.wert.b@gmail.com>
2022-04-02 15:29:17 -0400

    added units for the web app

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from enum import Enum, auto
+from flask import Flask, render_template
+import cv2
 
 class state(Enum):
     INIT = auto()
@@ -6,6 +8,31 @@ class state(Enum):
     FLOWERING = auto()
     HARVEST = auto()
 
+
+app = Flask(__name__)
+
+humidity = 65
+temperature = 10
+light_intensity = 5
+current_image = "./static/mushroom_1.png"
+
+image_path = './static/'
+current_timestep = 0
+
+@app.route('/1')
+def index():
+    return render_template(
+        'index.html',
+        humidity=humidity,
+        temperature=temperature,
+        light_intensity=light_intensity,
+        current_image=current_image
+    )
+
+def image_to_string(img):
+    new_image_path = image_path + str(current_timestep) + '.png'
+    cv2.imwrite(new_image_path, img)
+    current_image = format("<img src='{}' class='mushroom_image' height='10px' width='10px'>", new_image_path)
 
 def main():
     state = state.INIT
@@ -25,5 +52,5 @@ def main():
 
 
 if __name__ == '__main__':
-    state = state.INIT
+    app.run(debug=True, host='127.17.0.1')
     main()

a1de136c3ca526000a8da1500a8990e38f4f1aac
Nathaniel Wert <n8.wert.b@gmail.com>
2022-04-02 15:39:06 -0400

    added lcd display string update

--- main.py
+++ main.py
@@ -19,6 +19,8 @@ current_image = "./static/mushroom_1.png"
 image_path = './static/'
 current_timestep = 0
 
+lcd_display = ""
+
 @app.route('/1')
 def index():
     return render_template(
@@ -34,6 +36,9 @@ def image_to_string(img):
     cv2.imwrite(new_image_path, img)
     current_image = format("<img src='{}' class='mushroom_image' height='10px' width='10px'>", new_image_path)
 
+def update_lcd_display():
+    lcd_display = format("humidity: {}%\nTemperature: {}C\nLight Intensity: {}cd", humidity, temperature, light_intensity)
+
 def main():
     state = state.INIT
     while (True):
@@ -48,6 +53,7 @@ def main():
 
         elif state == state.HARVEST:
             pass
+    update_lcd_display()
 
 
 

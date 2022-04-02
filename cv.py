68fcb10f842dc9a2b9a08288ea1e6d93aa2e5c5b
ItchyCatIII <ItchyCatIII@users.noreply.github.com>
2022-04-02 15:06:10 -0400

    Flowering vs budding CV

--- cv.py
+++ cv.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 
 
-def find_mushrooms(img):
+def find_all_mushrooms(img):
     image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     bounds = ([0, 0, 0], [10, 255, 255])
     lower = np.array(bounds[0])
@@ -10,6 +10,21 @@ def find_mushrooms(img):
     mask = cv2.inRange(image, lower, upper)
     image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
     result = cv2.bitwise_and(image, image, mask=mask)
+    print(np.count_nonzero(result) / image.size)
     cv2.imshow('result', result)
     cv2.waitKey(5000)
     return np.count_nonzero(result) / image.size > 0.05
+
+
+def find_flowering_mushrooms(img):
+    image = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    bounds = ([0, 0, 0], [10, 255, 255])
+    lower = np.array(bounds[0])
+    upper = np.array(bounds[1])
+    mask = cv2.inRange(image, lower, upper)
+    image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
+    result = cv2.bitwise_and(image, image, mask=mask)
+    print(np.count_nonzero(result) / image.size)
+    cv2.imshow('result', result)
+    cv2.waitKey(5000)
+    return np.count_nonzero(result) / image.size > 0.3

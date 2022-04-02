e96b8e82cd66d81d7955666a57bfd840c13b709b
Nathaniel Wert <n8.wert.b@gmail.com>
2022-04-02 11:47:29 -0400

    structural init

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+from enum import Enum, auto
+
+class state(Enum):
+    INIT = auto()
+    BUDDING = auto()
+    FLOWERING = auto()
+    HARVEST = auto()
+
+
 def main():
-    pass
+    state = state.INIT
+    while (True):
+        if state == state.INIT:
+            pass
+
+        elif state == state.BUDDING:
+            pass
+
+        elif state == state.FLOWERING:
+            pass
+
+        elif state == state.HARVEST:
+            pass
+
 
 if __name__ == '__main__':
+    state = state.INIT
     main()

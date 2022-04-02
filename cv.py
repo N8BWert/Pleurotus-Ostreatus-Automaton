4fffe585c6cdf6f5b64dc9341021fa23ddff8e0c
ItchyCatIII <ItchyCatIII@users.noreply.github.com>
2022-04-02 14:30:14 -0400

    Remove extraneous CV output

--- cv.py
+++ cv.py
@@ -10,7 +10,6 @@ def find_mushrooms(img):
     mask = cv2.inRange(image, lower, upper)
     image = cv2.cvtColor(image, cv2.COLOR_HSV2BGR)
     result = cv2.bitwise_and(image, image, mask=mask)
-    print(np.count_nonzero(result) / image.size)
     cv2.imshow('result', result)
     cv2.waitKey(5000)
     return np.count_nonzero(result) / image.size > 0.05
